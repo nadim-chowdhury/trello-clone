e91a9f66632b8b40077f9ce3848e804b7e4561fd
Nadim <Nadim@chowdhury.com>
2023-11-22 01:49:42 +0600

    code updated

---
+++ lib/create-safe-action.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+export type FieldErrors<T> = {
+  [K in keyof T]?: string[];
+};
+
+export type ActionState<TInput, TOutput> = {
+  fieldErrors?: FieldErrors<TInput>;
+  error?: string | null;
+  data?: TOutput;
+};
+
+export const createSafeAction = <TInput, TOutput>(
+  shcema: z.Schema<TInput>,
+  handler: (validateData: TInput) => Promise<ActionState<TInput, TOutput>>
+) => {
+  return async (data: TInput): Promise<ActionState<TInput, TOutput>> => {
+    const validationResult = schema.safePerse(data);
+
+    if (!validationResult.success) {
+      fieldErrors: validationResult.error.flatten()
+        .FieldErrors as FieldErrors<TInput>;
+    }
+
+    return handler(validationResult.data);
+  };
+};
